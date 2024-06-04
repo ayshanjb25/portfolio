ccbd93c6729b14b774017368612c64e4f8de7eb3
Aisha Najeeb <90481018+ayshanjb25@users.noreply.github.com>
2024-06-05 00:36:24 +0530

    contact

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='max-container'>
+    <h1 className='head-text'>
+     My <span className='blue-gradient_text font-semibold drop-shadow'>Projects</span>
+    </h1>
+
+    <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+      <p> 
+        Software Engineer based in Sri Lanka, specializing in technical education through hands-on learning and building applications.
+      </p>
+    </div></section>
   )
 }
 
